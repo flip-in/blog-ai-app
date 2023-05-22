0bafe194f62a502f674b21681ed6ddb492d707b1
Will Price <pricew86@gmail.com>
2023-05-22 20:31:32 +0800

    integrate ai

---
+++ app/api/openai/route.ts
@@ -0,0 +1,42 @@
+import { Configuration, CreateChatCompletionResponse, OpenAIApi } from "openai"
+import { NextResponse } from "next/server";
+import { AxiosResponse } from "axios";
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export async function POST(request: Request, response: any) {
+  try {
+    const { title, role } = await request.json();
+
+    const aiResponse: AxiosResponse<CreateChatCompletionResponse, any> = 
+      await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: `Create a 3 line blog post with HTML tags based on this title: ${title}. Only provide the body of the article. Do not start with an H1 tag, and do not say "Sure thing"`,
+          },
+          {
+            role: "system",
+            content: `Your tone is :${role || "You are a helpful assistant"}. Write with HTML tags.`,
+          },
+        ],
+      });
+    
+    // response.revalidate("api/posts")
+    return NextResponse.json(
+      {
+        content: aiResponse.data.choices[0].message?.content,
+      }, 
+      { status: 200 }
+    );
+
+  } catch (error) {
+    console.error("request error", error);
+    NextResponse.json({ error: "error updating post" }, { status: 500 });
+  }
+}
